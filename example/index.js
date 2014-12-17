d35f459e13aba1e6276a492a66dc7d1d3e858129
Jason Brown <browniefed@gmail.com>
2014-12-17 09:52:23 -0800

    Add in document parsers.

--- example/index.js
+++ example/index.js
@@ -3,12 +3,19 @@ var JamaDoc = require('../index'),
     htmlparser = require('htmlparser2'),
     cheerio = require('cheerio');
 
-JamaDoc.parse('./Customer2.docx', function(convertedFile) {
-    var data = fs.readFileSync(convertedFile),
-        $ = cheerio.load(data.toString());
+JamaDoc.parse('./Customer4.docx', function(convertedDoc) {
 
-
+var toc = convertedDoc.parseTableOfContents();
+var headings = convertedDoc.parseHeadings();
+var content = convertedDoc.parseContent();
 //Detect table of contents
 //If none then start detecting hierarchy
 //We don't necessarily need headings we just need to identify hierarchy
+//
+//Table of contents doesn't have everything
+//--Get all h1,h2,h3,h4,h5, etc
+//---Get back to root node
+//----get content inbetween(all HTML)
+//----If the next node (h1=> h2) then we have a logical break
+//----This could match with table of contents but not necessarily
 });
