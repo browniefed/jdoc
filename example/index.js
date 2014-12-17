f7843ab9025e38e83398da100f06043a6819325a
Jason Brown <browniefed@gmail.com>
2014-12-16 22:05:59 -0800

    Just use pandoc to convert and not a buffer, then listen for close of spawned child

--- example/index.js
+++ example/index.js
@@ -1,5 +1,14 @@
-var JamaDoc = require('../index');
+var JamaDoc = require('../index'),
+    fs = require('fs-extra'),
+    htmlparser = require('htmlparser2'),
+    cheerio = require('cheerio');
 
-JamaDoc.parse('./Customer.docx', function() {
-    debugger;
+JamaDoc.parse('./Customer2.docx', function(convertedFile) {
+    var data = fs.readFileSync(convertedFile),
+        $ = cheerio.load(data.toString());
+
+
+//Detect table of contents
+//If none then start detecting hierarchy
+//We don't necessarily need headings we just need to identify hierarchy
 });
